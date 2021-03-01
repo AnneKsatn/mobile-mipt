2e8d495087fc83e87f1edf710fd14065f10c4dc6
Anna <AnneKsatn@gmail.com>
2021-03-01 11:47:41 +0100

    База готова, осталось отловить баги

--- SecondLab/SecondLab/ViewController.swift
+++ SecondLab/SecondLab/ViewController.swift
@@ -14,6 +14,99 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var resultLabel: UILabel!
+    var typingFlag = false
+    var firstOperand: Double = 0
+    var secondOpearnd: Double = 0
+    var operation: String = ""
+    var dotFlag = false;
+    
+    @IBAction func enterDigit(_ sender: UIButton) {
+        
+        let number = sender.currentTitle!
+        print(number)
+        
+        if(typingFlag) {
+            resultLabel.text = resultLabel.text! + number
+        } else {
+            resultLabel.text = number
+            typingFlag = true
+        }
+    }
+    
+    func beforeOperation() {
+        print("before")
+        firstOperand = Double(resultLabel.text!)!
+        typingFlag = false
+    }
+    
+    @IBAction func enterOperation(_ sender: UIButton) {
+        firstOperand = Double(resultLabel.text!)!
+        typingFlag = false
+        operation = sender.currentTitle!
+        dotFlag = false
+    }
+    
+    
+    @IBAction func enterEquality(_ sender: UIButton) {
+        
+        if(typingFlag == true) {
+            secondOpearnd = Double(resultLabel.text!)!
+        }
+        
+        switch operation {
+        case "+":
+            resultLabel.text = "\(secondOpearnd + firstOperand)"
+        case "-":
+            resultLabel.text = "\(firstOperand - secondOpearnd)"
+        case "*":
+            resultLabel.text = "\(firstOperand * secondOpearnd)"
+        case "/":
+            resultLabel.text = "\(firstOperand / secondOpearnd)"
+        default:
+            break
+        }
+        
+        typingFlag = false
+        dotFlag = false
+    }
+    
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        firstOperand = 0
+        secondOpearnd = 0
+        resultLabel.text = "0"
+        typingFlag = false
+        operation = ""
+        dotFlag = false
+    }
+    
+    @IBAction func changeNumSign(_ sender: UIButton) {
+        let newValue: Double =  -Double(resultLabel.text!)!
+        resultLabel.text = "\(newValue)"
+    }
+    
+    @IBAction func percentageButton(_ sender: UIButton) {
+        if(firstOperand == 0) {
+            resultLabel.text = "\(Double(resultLabel.text!)! / 100)"
+        } else {
+            
+        }
+    }
 
+    @IBAction func dotButton(_ sender: UIButton) {
+
+        if (typingFlag && !dotFlag) {
+            resultLabel.text = resultLabel.text! + "."
+            dotFlag = true
+        } else {
+            if(!typingFlag && !dotFlag) {
+                resultLabel.text = "0."
+                typingFlag = true
+                dotFlag = true
+            }
+        }
+        
+    }
 }
 
