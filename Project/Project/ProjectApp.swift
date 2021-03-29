bc280c822c5978498a84c616c6c10c57b8b6042f
AnneKsatn <36439886+AnneKsatn@users.noreply.github.com>
2021-03-29 08:12:07 +0100

    Fixed bug

--- Project/Project/ProjectApp.swift
+++ Project/Project/ProjectApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView(taskStore: <#T##TaskStore#>)
+            ContentView(taskStore: TaskStore())
         }
     }
 }
