ea9e8b7ea288e1243c5f8bc87c671063cc992660
AnneKsatn <AnneKsatn@gmail.com>
2021-05-31 16:15:45 +0300

    Добавлена AcivityLogin
    Внесено в манифест
    Сделан переход

---
+++ AppAndroid/app/src/main/java/com/example/appandroid/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.example.appandroid
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+
+}
--- AppAndroid/app/src/main/java/com/example/appandroid/MainActivity.kt
+++ AppAndroid/app/src/main/java/com/example/appandroid/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.appandroid
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,10 +13,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = email_registr.text.toString()
-        val password = password_registr.text.toString()
+        btn_registr.setOnClickListener {
+            val email = email_registr.text.toString()
+            val password = password_registr.text.toString()
 
-        Log.d("MainActivity", "Email is " + email)
-        Log.d("MainActivity", "Password " + password)
+            Log.d("MainActivity", "Email is " + email)
+            Log.d("MainActivity", "Password " + password)
+
+        }
+
+        have_account_text.setOnClickListener {
+            Log.d("MainActivity", "Show Login Page")
+
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
