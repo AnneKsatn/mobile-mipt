1c39399c77a842f1909c836207729758c0f15b24
AnneKsatn <AnneKsatn@gmail.com>
2021-06-01 01:23:08 +0300

    Настроены переходы

--- LastProject/app/src/main/java/com/example/lastproject/LoginActivity.kt
+++ LastProject/app/src/main/java/com/example/lastproject/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.example.lastproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_login.setOnClickListener {
+            val email_login = email_login.text.toString()
+            val password_login = password_login.text.toString()
+        }
+
+        go_to_redistr.setOnClickListener {
+            finish()
+        }
     }
 }
--- LastProject/app/src/main/java/com/example/lastproject/MainActivity.kt
+++ LastProject/app/src/main/java/com/example/lastproject/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.lastproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_registr.setOnClickListener{
+            val email = email_registr.text.toString()
+            val password = password_registr.text.toString()
+        }
+
+        go_to_login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
