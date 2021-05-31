0054eeb6f4c8532c7fc880d9c8b3417df087c55e
AnneKsatn <AnneKsatn@gmail.com>
2021-06-01 01:43:44 +0300

    Регистрация юзера через firebase

--- LastProject/app/src/main/java/com/example/lastproject/MainActivity.kt
+++ LastProject/app/src/main/java/com/example/lastproject/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.lastproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,9 +13,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_registr.setOnClickListener{
+        btn_registr.setOnClickListener {
             val email = email_registr.text.toString()
             val password = password_registr.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Пожалуйста, введите email и пароль", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) return@addOnCompleteListener
+
+                    Log.d("Main", "Successfully created user")
+
+                }
+                .addOnFailureListener {
+                    Log.d("MainActivity", "Ошибка создания пользователя")
+                }
         }
 
         go_to_login.setOnClickListener {
