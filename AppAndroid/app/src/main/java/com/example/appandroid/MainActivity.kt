ecdcf7d25996b2221a9ba7c4bed56f4b27e24eb8
AnneKsatn <AnneKsatn@gmail.com>
2021-05-31 15:49:44 +0300

    Разметка экрана регистрации

--- AppAndroid/app/src/main/java/com/example/appandroid/MainActivity.kt
+++ AppAndroid/app/src/main/java/com/example/appandroid/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.appandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email = email_registr.text.toString()
+        val password = password_registr.text.toString()
+
+        Log.d("MainActivity", "Email is " + email)
+        Log.d("MainActivity", "Password " + password)
     }
 }
